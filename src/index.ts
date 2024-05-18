dd37467d4785cbab000e5a56d1c5defae17e7a82
Taka005 <01taka005@gmail.com>
2024-05-18 23:40:43 +0000

    change:コマンドラインから実行可能に

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
+import { Command } from "commander";
 import puppeteer, { Browser } from "puppeteer";
 import Crawler from "./Crawler";
 
-(async()=>{
+type CrawlOption = {
+  url: string;
+}
+
+const program: Command = new Command();
+
+program
+  .option("-u, --url <url>","クロールするURL")
+  .action(async(options: CrawlOption)=>{
+    const url: string = options.url;
+
     const browser: Browser = await puppeteer.launch({
       args:["--lang=ja"]
     });
 
-    const crawler: Crawler = new Crawler(browser,"https://www.takasumibot.com/");
+    const crawler: Crawler = new Crawler(browser,url);
     await crawler.start();
-})()
+  });
+
+program.parse();
